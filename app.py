95f1fa637a6adf2c6ef47c692fd6a9353abdbc2c
Panduv <116863283+Pandurangaswamy867@users.noreply.github.com>
2025-02-27 12:18:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask,render_template
+from flask import Flask
+
 app = Flask(__name__)
-@app.route("/",methods=["GET","POST"])
-def start ():
-    return "Hello what ever"
-if __name__ =="__main__":
-     app.run(debug=True,host="0.0.0.0",port=2000)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return "Hello, this is an AWS App Runner Flask App!"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=2000)
