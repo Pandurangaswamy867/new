8d1d0960ab38da58b27596dbb7cb7fb67b1bfdde
Panduv <116863283+Pandurangaswamy867@users.noreply.github.com>
2025-02-27 11:52:24 +0530

    Update and rename server.js to requirements.txt

--- server.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 8080; // Default to 8080
-app.get('/', (req, res) => res.send('Hello from App Runner!'));
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
